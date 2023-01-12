38672a0a7c2863528e3ee17c68a746cb821415b6
Osvaldo Jimenez <ojimenez@pacific.edu>
2023-01-12 10:55:31 -0800

    added form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Task from "./components/Task";
+import { useState } from 'react' // highlight-line
+import Task from './components/Task'
 
-const App = ({tasks}) => {
+const App = (props) => {
+    const [tasks, setTasks] = useState(props.tasks)
+
+    const addTask = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+
     return (
         <div>
             <h1>Tasks</h1>
             <ul>
                 {tasks.map(task =>
-                    <Task key={task.id} task={task}/>
+                    <Task key={task.id} task={task} />
                 )}
             </ul>
+            <form onSubmit={addTask}>
+                <input />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }

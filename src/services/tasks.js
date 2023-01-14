e6f3805a01dce03caa099161f39aad1b701bc4f9
Osvaldo <ojimenez@pacific.edu>
2023-01-14 10:38:35 -0800

    made changes to work with backed on same site

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,16 +1,10 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/tasks'
+const baseUrl = '/api/tasks'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    const nonExisting = {
-        id: 10000,
-        content: 'This task is not saved to server',
-        date: '2023-01-10T17:30:31.098Z',
-        important: true,
-    }
-    return request.then(response => response.data.concat(nonExisting))
+    return request.then(response => response.data)
 }
 
 const create = newObject => {

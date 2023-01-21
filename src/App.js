cda79ddb311313edce1f44453afe4534c91bd5ed
Osvaldo <ojimenez@pacific.edu>
2023-01-21 00:19:21 -0800

    revamped tasks service to load auth tokens

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const App = () => {
             const user = await loginService.login({
                 username, password,
             })
-
+            taskService.setToken(user.token)
             setUser(user)
             setUsername('')
             setPassword('')

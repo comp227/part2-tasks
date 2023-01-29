0bf6538ced7e8f5bb19ae7e2e4366584af87dafc
Osvaldo <ojimenez@pacific.edu>
2023-01-28 16:02:57 -0800

    added a test for user events

--- src/components/Task.test.js
+++ src/components/Task.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Task from './Task';
 
 test('renders content', () => {
@@ -17,4 +18,23 @@ test('renders content', () => {
     expect(div).toHaveTextContent(
         'Component testing is done with react-testing-library'
     );
+});
+
+test('clicking the button calls event handler once', async () => {
+    const task = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    };
+
+    const mockHandler = jest.fn();
+
+    render(
+        <Task task={task} toggleImportance={mockHandler} />
+    );
+
+    const user = userEvent.setup();
+    const button = screen.getByText('make not important');
+    await user.click(button);
+
+    expect(mockHandler.mock.calls).toHaveLength(1);
 });

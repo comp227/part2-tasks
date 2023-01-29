745a1e66fc23fd8af99c4c1f9f9e140899e7e187
Osvaldo <ojimenez@pacific.edu>
2023-01-29 01:09:29 -0800

    removed logged-in hyphen

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ const App = () => {
             {!user && loginForm()}
             {user &&
                 <div>
-                    <p>{user.name} logged-in</p>
+                    <p>{user.name} logged in</p>
                     {taskForm()}
                 </div>
             }

c86eee713e72d2aa719f80595697ec8f1813f892
Osvaldo Jimenez <ojimenez@pacific.edu>
2024-02-29 20:08:54 -0800

    feat: update to 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import {useState, useEffect} from 'react'
-import Task from './components/Task'
+import {useState, useEffect } from "react";
+import Task from "./components/Task";
 import taskService from './services/tasks'
-import Notification from "./components/Notification";
-import Footer from "./components/Footer";
 import loginService from './services/login'
+import Notification from "./components/Notification.jsx";
+
+const Footer = () => {
+    const footerStyle = {
+        marginTop: 30,
+        paddingBottom: 15,
+        color: 'orange',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            Task app, Department of Computer Science, University of the Pacific
+        </div>
+    )
+}
 
 const App = () => {
     const [tasks, setTasks] = useState([])
     const [newTask, setNewTask] = useState('')
     const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState(null)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
-
-
+    
     useEffect(() => {
         taskService
             .getAll()
@@ -32,12 +47,38 @@ const App = () => {
         }
     }, [])
 
-    const addTask = event => {
+    console.log('rendered', tasks.length, 'tasks')
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+
+            window.localStorage.setItem(
+                'loggedTaskappUser', JSON.stringify(user)
+            )
+
+            taskService.setToken(user.token)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('Wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+    
+    const addTask = (event) => {
         event.preventDefault()
         const taskObject = {
             content: newTask,
             date: new Date().toISOString(),
-            important: Math.random() < 0.5,
+            important: Math.random() > 0.5,
         }
 
         taskService
@@ -53,16 +94,16 @@ const App = () => {
         setNewTask(event.target.value)
     }
 
-    const toggleImportanceOf = id => {
+    const toggleImportanceOf = (id) => {
         const task = tasks.find(t => t.id === id)
         const changedTask = { ...task, important: !task.important }
 
         taskService
             .update(id, changedTask)
             .then(returnedTask => {
-                setTasks(tasks.map(t => t.id !== id ? t : returnedTask))
+                setTasks(tasks.map(task => task.id !== id ? task : returnedTask))
             })
-            .catch(error => {
+            .catch(() => {
                 setErrorMessage(
                     `Task '${task.content}' was already deleted from server`
                 )
@@ -73,26 +114,7 @@ const App = () => {
             })
     }
 
-    const handleLogin = async (event) => {
-        event.preventDefault()
-        try {
-            const user = await loginService.login({
-                username, password,
-            })
-            window.localStorage.setItem(
-                'loggedTaskappUser', JSON.stringify(user)
-            )
-            taskService.setToken(user.token)
-            setUser(user)
-            setUsername('')
-            setPassword('')
-        } catch (exception) {
-            setErrorMessage('Wrong credentials')
-            setTimeout(() => {
-                setErrorMessage(null)
-            }, 5000)
-        }
-    }
+    const tasksToShow = showAll? tasks: tasks.filter(task => task.important)
 
     const loginForm = () => (
         <form onSubmit={handleLogin}>
@@ -127,15 +149,12 @@ const App = () => {
             <button type="submit">save</button>
         </form>
     )
-
-    const tasksToShow = showAll
-        ? tasks
-        : tasks.filter(task => task.important)
-
+    
     return (
         <div>
             <h1>Tasks</h1>
-            <Notification message={errorMessage} />
+            <Notification message={errorMessage}/>
+
             {!user && loginForm()}
             {user &&
                 <div>
@@ -143,15 +162,16 @@ const App = () => {
                     {taskForm()}
                 </div>
             }
+            
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
                 </button>
             </div>
             <ul>
-                {tasksToShow.map(task =>
+                {tasksToShow.map((task, i) =>
                     <Task
-                        key={task.id}
+                        key={i}
                         task={task}
                         toggleImportance={() => toggleImportanceOf(task.id)}
                     />
@@ -159,7 +179,7 @@ const App = () => {
             </ul>
             <Footer/>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
